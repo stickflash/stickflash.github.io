68c5576fba2930e56706e3d658e10f430c036a3e
s3alker s3alker <s3al4er@gmail.com>
2024-10-12 20:46:42 +0300

    Update

--- script.js
+++ script.js
@@ -1,77 +1,52 @@
 // Получаем элементы шагов
 const step1 = document.getElementById('step1');
 const step2 = document.getElementById('step2');
+const step3 = document.getElementById('step3');
 
 // Получаем кнопки
 const nextToStep2 = document.getElementById('nextToStep2');
-const backToStep1 = document.getElementById('backToStep1');
+const nextToStep3 = document.getElementById('nextToStep3');
+const backToStep2 = document.getElementById('backToStep2');
 
-// Получаем выпадающие списки выбора устройства и прошивки
-const deviceSelect = document.getElementById('deviceSelect');
+// Получаем выпадающий список выбора прошивки
 const firmwareSelect = document.getElementById('firmwareSelect');
 
-// Объект с прошивками для каждого устройства
-const firmwareOptions = {
-    M5StickCPlus2: ['Bruce', 'Nemo', 'Marauder', 'Shark'],
-    NodeMCU_ESP8266: ['Deauther']
-};
-
-// Функция для обновления списка прошивок в зависимости от выбранного устройства
-function updateFirmwareOptions() {
-    const selectedDevice = deviceSelect.value;
-    firmwareSelect.innerHTML = '';
-
-    firmwareOptions[selectedDevice].forEach(firmware => {
-        const option = document.createElement('option');
-        option.value = firmware;
-        option.textContent = firmware;
-        firmwareSelect.appendChild(option);
-    });
-}
-
-// Изначально обновляем список прошивок для выбранного по умолчанию устройства
-updateFirmwareOptions();
-
-// Обновляем прошивки при смене устройства
-deviceSelect.addEventListener('change', updateFirmwareOptions);
-
-// Переход к подключению устройства и прошивке (Шаг 1 -> Шаг 2)
+// Переход к выбору прошивки (Шаг 1 -> Шаг 2)
 nextToStep2.addEventListener('click', () => {
+    step1.classList.add('hidden'); // Скрываем первый шаг
+    step2.classList.remove('hidden'); // Показываем второй шаг
+});
+
+// Переход к подключению устройства и прошивке (Шаг 2 -> Шаг 3)
+nextToStep3.addEventListener('click', () => {
     let manifestUrl;
 
-    // В зависимости от выбранного устройства и прошивки устанавливаем соответствующий манифест
-    const device = deviceSelect.value;
-    const firmware = firmwareSelect.value;
-
-    if (device === 'M5StickCPlus2') {
-        switch (firmware) {
-            case 'Bruce':
-                manifestUrl = 'manifest_bruce.json';
-                break;
-            case 'Nemo':
-                manifestUrl = 'manifest_nemo.json';
-                break;
-            case 'Marauder':
-                manifestUrl = 'manifest_marauder.json';
-                break;
-            case 'Shark':
-                manifestUrl = 'manifest_shark.json';
-                break;
-        }
-    } else if (device === 'NodeMCU_ESP8266' && firmware === 'Deauther') {
-        manifestUrl = 'manifest_deauther.json';
+    // В зависимости от выбранной прошивки устанавливаем соответствующий манифест
+    switch (firmwareSelect.value) {
+        case 'Bruce':
+            manifestUrl = 'manifest_bruce.json';
+            break;
+        case 'Nemo':
+            manifestUrl = 'manifest_nemo.json';
+            break;
+        case 'Marauder':
+            manifestUrl = 'manifest_marauder.json';
+            break;
+        case 'Shark':
+            manifestUrl = 'manifest_shark.json';
+            break;
     }
 
     // Обновляем URL манифеста для кнопки прошивки
     const installButton = document.querySelector('esp-web-install-button');
     installButton.setAttribute('manifest', manifestUrl);
 
-    step1.classList.add('hidden'); // Скрываем первый шаг
-    step2.classList.remove('hidden'); // Показываем второй шаг
+    step2.classList.add('hidden'); // Скрываем второй шаг
+    step3.classList.remove('hidden'); // Показываем третий шаг
 });
 
-// Переход назад к выбору устройства и прошивки (Шаг 2 -> Шаг 1)
-backToStep1.addEventListener('click', () => {
-    step2.classList.add('hidden');
-    step1.classList.remove('hidden');
+// Переход назад к выбору прошивки (Шаг 3 -> Шаг 2)
+backToStep2.addEventListener('click', () => {
+    step3.classList.add('hidden');
+    step2.classList.remove('hidden');
 });
